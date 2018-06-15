8fc02974940d4f2139af1cf55ca6f70669642c93
Dan Reuman <reuman@ku.edu>
2018-06-15 09:03:23 -0500

    more initial files

---
+++ R/plotphase.wpmf.R
@@ -0,0 +1,4 @@
+#maybe a plotphase methods? And then a plot method that just calls the plotmag and plotphase methods?
+#not clear if this is needed, if there are no additional features the method could just be 
+#inherited from tts. But the confidence contours may also make sense here in addition to 
+#plotmag.wpmf, so maybe it does make sense to have this instead of inheriting from tts.
---
+++ R/plotphase.wt.R
@@ -0,0 +1,3 @@
+#maybe a plotphase methods? And then a plot method that just calls the plotmag and plotphase methods?
+#not clear if this is needed, if there are no additional features the method could just be 
+#inherited from tts
