66b394d0a3d129e64fee9c2cd2e91718a5216fc5
Dan Reuman <reuman@ku.edu>
2018-06-15 09:52:32 -0500

    error checking for tts objects

--- R/errcheck_times.R
+++ R/errcheck_times.R
@@ -12,19 +12,19 @@ errcheck_times<-function(times,callfunc)
 {
   if (!is.numeric(times))
   {
-    stop(paste0("Error in ",callfunc,": times must be numeric"))
+    stop(paste0("Error in errcheck_times called by ",callfunc,": times must be numeric"))
   }
   if (length(times)<2)
   {
-    stop(paste0("Error in ",callfunc,": times must be a vector"))
+    stop(paste0("Error in errcheck_times called by ",callfunc,": times must be a vector"))
   }
   d<-diff(times)
   if (!isTRUE(all.equal(rep(d[1],length(d)-1),d[2:length(d)])))
   {
-    stop(paste0("Error in ",callfunc,": times must be evenly spaced"))  
+    stop(paste0("Error in errcheck_times called by ",callfunc,": times must be evenly spaced"))  
   }
   if (d[1]<=0)
   {
-    stop(paste0("Error in "callfunc,": times must be increasing"))
+    stop(paste0("Error in errcheck_times called by "callfunc,": times must be increasing"))
   }
 }
---
+++ R/errcheck_tts.R
@@ -0,0 +1,44 @@
+#' Error check whether inputs are suitable for a tts object
+#' 
+#' @param times times of measurement
+#' @param timescale timescales of analysis
+#' @param values a times by timescales matrix
+#' @param callfunc the function from which this one was called, for error tracking
+#' 
+#' @return \code{errcheck_tts} returns nothing but throws and error if inputs not appropriate
+#' 
+#' @author Daniel Reuman, \email{reuman@@ku.edu}
+
+errcheck_tts<-function(times,timescales,values,callfunc)
+{
+  errcheck_times(times,callfunc)
+  
+  #timescale should be a numeric vector with positive entries 
+  if (!is.numeric(timescales))
+  {
+    stop(paste0("Error in errcheck_tts called by ",callfunc,": timescales must be numeric"))
+  }
+  if (length(timescales)<2)
+  {
+    stop(pasteo("Error in errcheck_tts called by ",callfunc,": timescales must be a vector"))
+  }
+  if (any(timescales<=0))
+  {
+    stop(paste0("Error in errcheck_tts called by ",callfunc,": timescales must be positive"))
+  }
+  #***DAN: decreasing/increasing, what? add this once you figure it out
+  
+  #values a numeric or complex-valued matrix, length(times) by length(timescales)
+  if (!is.numeric(values) && !is.complex(values))
+  {
+    stop(paste0("Error in errcheck_tts called by ",callfunc,": values must be numeric or complex"))
+  }
+  if (!is.matrix(values))
+  {
+    stop(paste0("Error in errcheck_tts called by ",callfunc,": values must be a matrix"))
+  }
+  if (dim(values)[1]!=length(times) || dim(values)[2]!=length(timescales))
+  {
+    stop(paste0("Error in errcheck_tts called by ",callfunc,": dimensions of values should agree with lengths of times and timescales"))
+  }
+}
