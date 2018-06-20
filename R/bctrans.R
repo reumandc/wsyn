bcb446a77bed60f3a9df9975f3b1cc71b45ccad1
Dan Reuman <reuman@ku.edu>
2018-06-20 11:35:38 -0500

    box cox transformation function and unit tests

---
+++ R/bctrans.R
@@ -0,0 +1,25 @@
+#' The one-parameter family of Box-Cox transformations
+#' 
+#' @param y A numeric, positive values assumed
+#' @param lambda The Box-Cox parameter
+#' 
+#' @return \code{bctrans} gives \code{((y^lambda)-1)/lambda} for \code{lambda} not 0 or \code{ln(y)} for \code{lambda} equal to 0.
+#' 
+#' @details No error checking done, so it is assumed the entries of y are positive.
+#'  
+#' @references 
+#' Box, GEP and Cox, DR (1964) An analysis of transformations (with discussion). Journal of the Royal Statistical Society B, 26, 211–252.
+#' Venables, WN and Ripley, BD (2002) Modern Applied Statistics with S. Fourth edition. Springer.
+#' 
+#' @author Daniel Reuman, \email{reuman@@ku.edu}
+
+bctrans<-function(y,lambda)
+{
+  if (isTRUE(all.equal(lambda,0)))
+  {
+    return(log(y))
+  }else
+  {
+    return(((y^lambda)-1)/lambda)
+  }
+}
