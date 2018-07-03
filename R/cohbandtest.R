36dba29e19bb0aa28c925f804a3812c2415028e4
Dan Reuman <reuman@ku.edu>
2018-07-03 08:41:48 -0500

    added a functions that computed significance of coherence over a timescale band

---
+++ R/cohbandtest.R
@@ -0,0 +1,86 @@
+#' Aggregate significance of coherence across a timescale band
+#' 
+#' Computes the aggregate significance of coherence across a timescale band, accounting for
+#' non-independence of timescales
+#' 
+#' @param cohobj An object of class \code{coh}, must have a non-\code{NA} \code{signif} slot
+#' @param band A length-two numeric vector indicating a timescale band
+#' 
+#' @result \code{cohbandtest} returns an object of the same class as its first input but with a
+#' \code{bandp} object added. Or if there was already a \code{bandp} object, the output has a 
+#' \code{bandp} object with an additional row. The \code{bandp} object is a data frame with three
+#' columns, the first two indicating the timescale band and the third an associated p-value for
+#' the test of coherence over that band.
+#' 
+#' @author Thomas Anderson, \email{anderstl@@gmail.com}, Jon Walter, \email{jaw3es@@virginia.edu}; Lawrence 
+#' Sheppard, \email{lwsheppard@@ku.edu}; Daniel Reuman, \email{reuman@@ku.edu}
+#' 
+#' @references 
+#' Sheppard, L.W., et al. (2016) Changes in large-scale climate alter spatial synchrony of aphid 
+#' pests. Nature Climate Change. DOI: 10.1038/nclimate2881
+#' 
+#' @exportMethod 
+
+cohbandtest<-function(object,...)
+{
+  UseMethod("cohbandtest",object)
+}
+
+#' @rdname cohbandtest
+#' @export
+cohbandtest.default<-function(object,...)
+{
+  stop("Error in cohbandtest: method not defined for this class")
+}
+
+#' @rdname cohbandtest
+#' @export
+cohbandtest.coh<-function(cohobj,band)
+{
+  #error checking
+  if (any(is.na(cohobj$signif)))
+  {
+    stop("Error in cohbandtest: signif cannot be NA")
+  }
+  if (!is.numeric(band))
+  {
+    stop("Error in cohbandtest: band must be numeric")
+  }
+  if (!is.vector(band))
+  {
+    stop("Error in cohbandtest: band must be a length-two numeric vector")
+  }
+  if (length(band)!=2)
+  {
+    stop("Error in cohbandtest: band must be a length-two numeric vector")
+  }
+  band<-sort(band)
+  timescales<-get_timescales(cohobj)
+  if (band[1]>max(timescales) || band[2]<min(timescales))
+  {
+    stop("Error in cohbandtest: band must include some of the timescales")
+  }
+  
+  #add ranks if necessary
+  if (any(is.na(cohobj$ranks)))
+  {
+    cohobj<-addranks(cohobj)
+  }
+  
+  #get the p-value
+  x<-mean(cohobj$ranks$coher[timescales>=band[1] & timescales<=band[2]]) #mean rank across timescales of interest, data
+  sx<-apply(FUN=mean,X=cohobj$ranks$scoher[,timescales>=band[1] & timescales<=band[2],drop=F],MARGIN=1) #mean ranks, surrogates
+  pval<-(sum(sx>=x)+1)/(length(sx)+1)
+  
+  #form the result and return it
+  if (any(is.na(cohobj$bandp)))
+  {
+    bandp<-data.frame(ts_low_bd=band[1],ts_hi_bd=band[2],p_val=pval)    
+    cohobj$bandp<-bandp
+    return(cohobj)
+  }else
+  {
+    cohobj$bandp[dim(cohobj$bandp)[1]+1,]<-c(band,pval)
+    return(cohobj)
+  }
+}
---
+++ tests/testthat/tests_cohbandtest.R
@@ -0,0 +1,73 @@
+context("cohbandtest")
+
+test_that("test the default method, which just throws an error",{
+  object<-list("test","this","is")
+  expect_error(cohbandtest(object),"Error in cohbandtest: method not defined for this class")
+})
+
+test_that("test for a coh object",{
+  #this test based on supplementary figure 5 in Sheppard et al, Nature Climate Change, 
+  #2016, doi: 10.1038/NCLIMATE2881. 
+  
+  #make the data
+  set.seed(101)
+  times<-(-3:100)
+  ts1<-sin(2*pi*times/10)
+  ts2<-5*sin(2*pi*times/3)
+  artsig_x<-matrix(NA,11,length(times)) #the driver
+  for (counter in 1:11)
+  {
+    artsig_x[counter,]=ts1+ts2+rnorm(length(times),mean=0,sd=1.5)
+  }
+  times<-0:100
+  artsig_y<-matrix(NA,11,length(times)) #the driven
+  for (counter1 in 1:11)
+  {
+    for (counter2 in 1:101)
+    {
+      artsig_y[counter1,counter2]<-mean(artsig_x[counter1,counter2:(counter2+2)])
+    }
+  }
+  artsig_y<-artsig_y+matrix(rnorm(length(times)*11,mean=0,sd=3),11,length(times))
+  artsig_x<-artsig_x[,4:104]
+  artsig_x<-cleandat(artsig_x,times,1)$cdat
+  artsig_y<-cleandat(artsig_y,times,1)$cdat
+  
+  #call coh
+  cohobj<-coh(dat1=artsig_x,dat2=artsig_y,times=times,norm="powall",sigmethod="none",nrand=1000,
+           f0=0.5,scale.max.input=28)
+  
+  #test error handling
+  expect_error(cohbandtest(cohobj,c(2,10)),"Error in cohbandtest: signif cannot be NA")
+  cohobj<-coh(dat1=artsig_x,dat2=artsig_y,times=times,norm="powall",sigmethod="fast",nrand=1000,
+              f0=0.5,scale.max.input=28)
+  expect_error(cohbandtest(cohobj,"test"),"Error in cohbandtest: band must be numeric")  
+  expect_error(cohbandtest(cohobj,5),"Error in cohbandtest: band must be a length-two numeric vector")  
+  expect_error(cohbandtest(cohobj,c(1,2,5)),"Error in cohbandtest: band must be a length-two numeric vector")  
+  expect_error(cohbandtest(cohobj,c(31,35)),"Error in cohbandtest: band must include some of the timescales")  
+  
+  #test timescales <=4 years
+  cohobj<-cohbandtest(cohobj,c(2,4))
+  expect_equal(class(cohobj$bandp),"data.frame")
+  expect_equal(dim(cohobj$bandp),c(1,3))
+  expect_gt(cohobj$bandp[1,3],.2)
+  expect_equal(cohobj$bandp[1,1],2)
+  expect_equal(cohobj$bandp[1,2],4)
+  
+  #test timescales > 4 years
+  cohobj<-cohbandtest(cohobj,c(4.001,30))
+  expect_equal(class(cohobj$bandp),"data.frame")
+  expect_equal(dim(cohobj$bandp),c(2,3))
+  expect_lt(cohobj$bandp[2,3],.01)
+  expect_equal(cohobj$bandp[2,1],4.001)
+  expect_equal(cohobj$bandp[2,2],30)
+  
+  #test case with only one timescale in the range
+  cohobj<-cohbandtest(cohobj,c(10,10.1))
+  expect_equal(class(cohobj$bandp),"data.frame")
+  expect_equal(dim(cohobj$bandp),c(3,3))
+  expect_lt(cohobj$bandp[3,3],.01)
+  expect_equal(cohobj$bandp[3,1],10)
+  expect_equal(cohobj$bandp[3,2],10.1)
+})
+
