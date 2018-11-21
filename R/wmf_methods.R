8ddee317a60094638c6d7b9a68ee8590c95c2864
Dan Reuman <reuman@ku.edu>
2018-11-20 21:11:34 -0600

    added print method for wmf class and tests

--- R/wmf_methods.R
+++ R/wmf_methods.R
@@ -48,18 +48,45 @@ get_wtopt.wmf<-function(obj)
   return(obj$wtopt)
 }
 
-#This is based on an existing generic so not sure if this will work like this.
-#Also, might not be necessary, perhaps the tts one is enough.
-#summary.wmf<-function(obj)
-#{
-#  
-#}
+#' @export
+print.wmf<-function(x,...)
+{
+  cat("wmf object:\n")
+  
+  cat("times, a length",length(x$times),"numeric vector:\n")
+  if (length(x$times)<12)
+  {
+    cat(paste(x$times),"\n")  
+  }else
+  {
+    cat(paste(x$times[1:5]),"...",paste(x$times[(length(x$times)-4):(length(x$times))]),"\n")
+  }
+  
+  cat("Number of sampling locations:",dim(x$dat)[1],"\n")
+  
+  cat("timescales, a length",length(x$timescales),"numeric vector:\n")
+  if (length(x$timescales)<12)
+  {
+    cat(paste(x$timescales),"\n")  
+  }else
+  {
+    cat(paste(x$timescales[1:5]),"...",paste(x$timescales[(length(x$timescales)-4):(length(x$timescales))]),"\n")
+  }
+  
+  if (length(x$timescales)<=5 && length(x$times)<=5)
+  {
+    cat("values, a",dim(x$values)[1],"by",dim(x$values)[2]," matrix:\n")
+    print(x$values)
+  }else
+  {
+    cat("values, a",dim(x$values)[1],"by",dim(x$values)[2],"matrix, upper left is:\n")
+    print(x$values[1:5,1:5])
+  }
+  
+  w<-x$wtopt
+  cat("wtopt:\n")
+  cat("scale.min=",w$scale.min,"; scale.max.input=",w$scale.max.input,"; sigma=",w$sigma,"; f0=",w$f0,sep="")
+}
 
-#This is based on an existing generic so not sure if this will work like this.
-#Also, might not be necessary, perhaps the tts one is enough.
-#print.wmf<-function(obj)
-#{
-#  
-#}
+#write a summary method when the time comes
 
-#What else? 
--- tests/testthat/tests_wmf_methods.R
+++ tests/testthat/tests_wmf_methods.R
@@ -2,4 +2,15 @@ context("wmf_methods")
 
 #tests of set and get functions considered unnecessary
 
-#when print and summary functions written, add tests
+test_that("test print.wmf", {
+  set.seed(101)
+  
+  times<-1:35
+  dat<-matrix(rnorm(10*length(times)),10,length(times))
+  dat<-cleandat(dat,times,1)$cdat
+  obj1<-wmf(dat,times)
+  
+  expect_equal(capture_output(print(obj1)),"wmf object:\ntimes, a length 35 numeric vector:\n1 2 3 4 5 ... 31 32 33 34 35 \nNumber of sampling locations: 10 \ntimescales, a length 41 numeric vector:\n2 2.1 2.205 2.31525 2.4310125 ... 11.5836322719437 12.1628138855409 12.770954579818 13.4095023088089 14.0799774242493 \nvalues, a 35 by 41 matrix, upper left is:\n                 [,1]                  [,2]                  [,3]\n[1,]         NaN+NaNi        NaN+      NaNi        NaN+      NaNi\n[2,]         NaN+NaNi        NaN+      NaNi        NaN+      NaNi\n[3,]         NaN+NaNi        NaN+      NaNi        NaN+      NaNi\n[4,]  0.07788241-  0i  0.0762725+0.1036329i  0.0943894+0.1989645i\n[5,] -0.20361809-  0i -0.1994417-0.0425929i -0.2279712-0.0816981i\n                      [,4]                  [,5]\n[1,]        NaN+      NaNi        NaN+      NaNi\n[2,]        NaN+      NaNi        NaN+      NaNi\n[3,]        NaN+      NaNi        NaN+      NaNi\n[4,]  0.1268653+0.2797649i  0.1620964+0.3384150i\n[5,] -0.2800554-0.1122587i -0.3356957-0.1299196i\nwtopt:\nscale.min=2; scale.max.input=; sigma=1.05; f0=1")
+})
+
+#when summary function written, add tests
