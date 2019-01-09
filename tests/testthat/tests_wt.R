84d8c1e5f62979b89f933b1569e9a338a4f22843
Dan Reuman <reuman@ku.edu>
2019-01-09 16:11:51 -0600

    new tests for wt for non standard values of f0, sigma, scale.min, scale.max.input

--- tests/testthat/tests_wt.R
+++ tests/testthat/tests_wt.R
@@ -20,9 +20,8 @@ test_that("other tests that output has the correct format",{
   expect_equal(dim(res$values)[1],length(times))
   expect_equal(dim(res$values)[2],length(res$timescales))
   expect_equal(class(res$values[50,50]),"complex")
+  expect_equal(res$timescales[1:10],2*1.05^(0:9))
 })
-
-#maybe add some tests of the timescale output
 
 test_that("test for qualitatively correct output, test 1",{
   #this test based on the first example in fig S2 of Sheppard et al, "Synchrony is more than its
@@ -73,3 +72,76 @@ test_that("test for qualitatively correct output, test 2",{
   #It looked good so I commented it out, now just check future runs are always the same.
   expect_known_value(res,file="../vals/wt_testval_02",update=FALSE)
 })
+
+test_that("test for non-default values of f0, sigma, scale.min, scale.max.input",{
+  
+  #do a test where you expand scale.max.input to a large values and compare with NULL
+  #and find the same thing on the overlapping part
+  times<-1:100
+  t.series<-rnorm(length(times))
+  t.series<-t.series-mean(t.series)
+  res1<-wt(t.series, times)
+  res2<-wt(t.series, times, scale.max.input = 1000)
+  
+  expect_equal(res1$times,res2$times)
+  
+  numnonnas1<-apply(FUN=sum,X=is.finite(res1$values),MARGIN=2)
+  ts1<-res1$timescales[numnonnas1>0]
+  numnonnas2<-apply(FUN=sum,X=is.finite(res2$values),MARGIN=2)
+  ts2<-res2$timescales[numnonnas2>0]
+  expect_equal(numnonnas1,numnonnas2[1:length(numnonnas1)])
+  expect_equal(ts1,ts2)
+  
+  expect_equal(res1$values,res2$values[,numnonnas2>0])
+  
+  #do a test where you try a couple different values of f0
+  set.seed(201)
+  timeinc<-1 #one sample per year
+  startfreq<-0.2 #cycles per year
+  endfreq<-0.1 #cycles per year
+  times<-1:200
+  f<-seq(from=startfreq,by=(endfreq-startfreq)/(length(times)-1),to=endfreq) #frequency for each sample
+  phaseinc<-2*pi*cumsum(f*timeinc)
+  t.series<-sin(phaseinc)
+  t.series<-t.series-mean(t.series)
+
+  res0p5<-wt(t.series, times, f0=0.5)
+  #plotmag(res0p5)
+  #lines(range(res1$times),log2(c(5,5)),type='l')
+  #lines(range(res1$times),log2(c(10,10)),type='l')
+
+  res1<-wt(t.series, times)
+  #plotmag(res1)
+  #lines(range(res1$times),log2(c(5,5)),type='l')
+  #lines(range(res1$times),log2(c(10,10)),type='l')
+  
+  res2<-wt(t.series, times, f0=2)
+  #plotmag(res2)
+  #lines(range(res1$times),log2(c(5,5)),type='l')
+  #lines(range(res1$times),log2(c(10,10)),type='l')
+  
+  #checked plots looked right, now comment out the plot lines and use expect_known_value
+  expect_known_value(res0p5$values,file="../vals/wt_testval_03",update=FALSE)
+  expect_known_value(res1$values,file="../vals/wt_testval_04",update=FALSE)
+  expect_known_value(res2$values,file="../vals/wt_testval_05",update=FALSE)
+
+  #do a test where you try a different value of scale.min
+  res1<-wt(t.series, times)
+  res2<-wt(t.series, times, scale.min=2*1.05^5)
+  expect_equal(res1$times,res2$times)
+  expect_equal(res1$timescales[6:length(res1$timescales)],res2$timescales)
+  expect_equal(res1$values[,6:length(res1$timescales)],res2$values)  
+  
+  #test non-default values of sigma
+  res1<-wt(t.series, times, sigma=1.05, f0=3)
+  res2<-wt(t.series, times, sigma=1.01, f0=3)
+  expect_equal(res1$timescales[1:10],2*1.05^c(0:9))
+  expect_equal(res2$timescales[1:10],2*1.01^c(0:9))
+
+  #plotmag(res1)
+  #plotmag(res2)
+  #checked these look right, now comment out the plot lines and use expect_known_value
+  expect_known_value(res1$values,file="../vals/wt_testval_06",update=FALSE)
+  expect_known_value(res2$values,file="../vals/wt_testval_07",update=FALSE)
+})
+
